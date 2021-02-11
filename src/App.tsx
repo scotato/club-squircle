eab051f6c0c795051200ea327e365879e64cc497
Scott Dodge <scott@goldilocks.design>
2021-02-10 21:07:47 -0600

    add squircle

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Squircle } from "@scotato/react-squircle";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Squircle c={10} width={256} height={256} />;
 }
 
 export default App;
