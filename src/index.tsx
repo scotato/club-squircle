8fcb79e4c9cfdb263b91487a91fc759355409cc1
Scott Dodge <scott@goldilocks.design>
2021-02-14 15:17:35 -0600

    move components and logos into directories

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import App from "./App";
+import App from "./components/App";
 import theme from "./theme";
 import reportWebVitals from "./reportWebVitals";
 
