73c73a58f61977fb1cb112fe23cab510517a60d2
Scott Dodge <scott@goldilocks.design>
2021-02-17 20:59:31 -0600

    update intro layout

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Text, Link } from "@chakra-ui/react";
+import { Box, Stack, Text, Link } from "@chakra-ui/react";
 import { ReactComponent as SquircleWordmark } from "../logos/squircle-wordmark.svg";
 import { repository } from "../../package.json";
 
@@ -18,21 +18,22 @@ const Reference = ({ to, text }: ReferenceProps) => (
 );
 
 const Introduction = () => (
-  <Box p={2} py={4}>
+  <Stack mb={2} p={2} spacing={4}>
     <Box
-      as={SquircleWordmark}
       title="Squircle"
+      as={SquircleWordmark}
       sx={{ path: { fill: "currentColor" } }}
-      mb={4}
     />
 
     <Text>
-      A squircle is an intermediate shape between a square and a circle. This{" "}
-      <Reference to={repository} text="open source" /> tool helps you to create{" "}
-      <Reference to={SquircleUrl} text="squircles" /> and{" "}
+      A squircle is an intermediate shape between a square and a circle.
+    </Text>
+    <Text>
+      This <Reference to={repository} text="open source" /> tool helps you
+      create <Reference to={SquircleUrl} text="squircles" /> and{" "}
       <Reference to={SuperellipseUrl} text="superellipses" />.
     </Text>
-  </Box>
+  </Stack>
 );
 
 export default Introduction;
